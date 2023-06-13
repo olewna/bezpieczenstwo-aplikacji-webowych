40c4cec47f1a43978912bc472ad14fa1108798b2
olewna <dajcie.suba2002@gmail.com>
2023-06-13 15:01:58 +0200

    keycloak

--- lab04/src/Keycloak.js
+++ lab04/src/Keycloak.js
@@ -1,8 +1,41 @@
 import Keycloak from "keycloak-js";
+
+function generateCodeVerifier() {
+  const codeVerifierLength = 128;
+  const charset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+  let codeVerifier = "";
+
+  for (let i = 0; i < codeVerifierLength; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    codeVerifier += charset.charAt(randomIndex);
+  }
+
+  return codeVerifier;
+}
+
+function generateCodeChallenge(codeVerifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const buffer = base64UrlEncode(data);
+  return buffer;
+}
+
+function base64UrlEncode(input) {
+  const base64 = btoa(String.fromCharCode.apply(null, input));
+  return base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+}
+
+const codeVerifier = generateCodeVerifier();
+const codeChallenge = generateCodeChallenge(codeVerifier);
+
 const keycloak = new Keycloak({
   url: "http://localhost:8080/",
   realm: "lab04",
-  clientId: "myclient",
+  clientId: "myclient2",
+  pkceMethod: "S256",
+  code_verifier: codeVerifier,
+  code_challenge: codeChallenge,
 });
 
 export default keycloak;
